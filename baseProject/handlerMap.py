301f1890e15237f1f35969a3586f18f40921098a
Costa Halicea <costa@halicea.com>
2011-11-19 06:06:04 +0200

    .halProject added in the tests path so i can load the test project from there

--- baseProject/handlerMap.py
+++ baseProject/handlerMap.py
@@ -1,5 +1,4 @@
 #{%block imports%}
-from Controllers import BaseControllers
 from Controllers import ShellControllers
 from Controllers import HalWebControllers
 #{%endblock%}
@@ -7,17 +6,6 @@ webapphandlers = [
 #{%block ApplicationControllers %}
 
 
-#{%block BaseControllers %}
-('/Login', BaseControllers.LoginController),
-('/Login/(.*)', BaseControllers.LoginController),
-('/Logout',BaseControllers.LogoutController),
-('/AddUser', BaseControllers.AddUserController),
-('/WishList', BaseControllers.WishListController),
-('/admin/Role', BaseControllers.RoleController),
-('/admin/RoleAssociation', BaseControllers.RoleAssociationController),
-('/Base/WishList', BaseControllers.WishListController),
-('/Base/Invitation', BaseControllers.InvitationController),
-#{%endblock%}
 
 #{%block ShellControllers%}
 ('/admin/Shell', ShellControllers.FrontPageController),
