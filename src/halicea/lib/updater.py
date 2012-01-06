aa8540b2465d3e7a8543206b263037f4ca6fb45e
Costa Halicea <costa@halicea.com>
2012-01-06 15:41:13 +0200

    added new module update.py which will handle the project and framework updates

---
+++ src/halicea/lib/updater.py
@@ -0,0 +1,16 @@
+"""
+Functions to check and update the framework or project packages
+"""
+
+def check_framework_updates():
+    """cheks if the framework has a new version"""
+    pass
+def check_project_updates():
+    """checks if the halicea package has some new version"""
+    pass
+def update_project():
+    """updates a package from the repository"""
+    pass
+def update_framework():
+    """updates the framework to the latest version"""
+    pass
